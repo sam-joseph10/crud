b221c2e81dbdc6e1379a3df5777d27618d512d94
M K Samuel Joseph <mksamjoseph29@gmail.com>
2025-10-06 12:09:41 +0530

    Fix template issue for Render

--- crud/settings.py
+++ crud/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',   # for development
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for production (Render)
+
 
 LOGIN_URL="login"
 # Default primary key field type
